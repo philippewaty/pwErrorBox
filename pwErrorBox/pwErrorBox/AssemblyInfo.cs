8ce242d9db03e4ffb359b2db472ce8aaf9c6c921
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2025-10-27 10:46:50 +0100

    Replace txtAssembly by datagridview

---
+++ pwErrorBox/pwErrorBox/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pwErrorBox
+{
+    /// <summary>
+    /// Information about an assembly.
+    /// </summary>
+    internal class AssemblyInfo
+    {
+        public string Name { get; set; }
+        public string ImageRuntimeVersion { get; set; }
+        public string Location { get; set; }
+    }
+}
