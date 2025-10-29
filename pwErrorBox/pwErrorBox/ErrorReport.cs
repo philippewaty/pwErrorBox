c806b57d5c3c6aca05ed5f2baaf2702ffc831055
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2025-10-29 12:36:19 +0100

    Code cleaning

--- pwErrorBox/pwErrorBox/ErrorReport.cs
+++ pwErrorBox/pwErrorBox/ErrorReport.cs
@@ -36,39 +36,9 @@ namespace pwErrorBox
     public CultureInfo CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
 
     /// <summary>
-    /// Gets or sets name or IP address of the SMTP Host.
-    /// </summary>
-    public string SmtpHost;
-
-    /// <summary>
-    /// Gets or sets if the SMTP uses SSL.
-    /// </summary>
-    public bool EnableSSL;
-
-    /// <summary>
-    /// Gets or sets the port used for SMTP.
-    /// </summary>
-    public int Port = 25;
-
-    /// <summary>
-    /// Gets or sets the username used for SMTP.
-    /// </summary>
-    public string UserName = "";
-
-    /// <summary>
-    /// Gets or sets the password used for SMTP. 
-    /// </summary>
-    public string Password = "";
-
-    /// <summary>
     /// Gets or sets email address to receive the error report.
     /// </summary>
     public string ToEmail;
-
-    /// <summary>
-    /// Gets or sets email address of the error report sender.
-    /// </summary>
-    public string FromEmail;
 
     /// <summary>
     /// Creates an instance of this class if not already created.
