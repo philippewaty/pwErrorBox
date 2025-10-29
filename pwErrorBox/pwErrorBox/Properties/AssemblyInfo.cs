0632a7761aa4e7bddc65b10b6854ddad73ea095d
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2025-10-29 12:38:29 +0100

    Update AssemblyInfo

--- pwErrorBox/pwErrorBox/Properties/AssemblyInfo.cs
+++ pwErrorBox/pwErrorBox/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("pwErrorBox")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Display error box")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("pwErrorBox")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
